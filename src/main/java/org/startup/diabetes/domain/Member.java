8fce185935fadd94237a351508f6cb354b2f45e4
ruah0807 <kimhk0315@gmail.com>
2024-02-22 20:51:36 +0900

    Set up Member Entity in domain
    id
    nickname
    pw
    name
    age
    gender
    tall
    weight

---
+++ src/main/java/org/startup/diabetes/domain/Member.java
@@ -0,0 +1,41 @@
+package org.startup.diabetes.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String pw;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(nullable = false)
+    private int tall;
+
+    @Column(nullable = false)
+    private int weight;
+
+}
