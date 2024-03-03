35bef78015fb54d0e9c08abe7407424aebb154f6
ruah0807 <kimhk0315@gmail.com>
2024-03-03 23:31:11 +0900

    사용자 정보 조회 (마이페이지)

---
+++ src/main/java/org/startup/diabetes/security/MemberSecurityDTO.java
@@ -0,0 +1,43 @@
+package org.startup.diabetes.security;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@ToString
+public class MemberSecurityDTO  extends User {
+
+    private Long id;
+
+    private String userid;
+
+    private String pw;
+
+    private String name;
+
+    private int age;
+
+    private String gender;
+
+    private int tall;
+
+    private int weight;
+
+    public MemberSecurityDTO(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.userid = username;
+        this.pw = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userid;
+    }
+}
--- src/main/java/org/startup/diabetes/security/UserDetailService.java
+++ src/main/java/org/startup/diabetes/security/UserDetailService.java
@@ -5,7 +5,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.startup.diabetes.domain.Member;
 import org.startup.diabetes.repository.MemberRepository;
+
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserDetailService implements UserDetailsService {
@@ -14,6 +18,13 @@ public class UserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userid) throws UsernameNotFoundException {
+
+        Optional<Member> member = memberRepository.findByUserid(userid);
+
+        if(member.isEmpty()){
+            throw new UsernameNotFoundException("Userid Not Found");
+        }
+
         return memberRepository.findByUserid(userid)
                 .orElseThrow(() -> new IllegalArgumentException((userid)));
     }
