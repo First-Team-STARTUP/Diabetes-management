142db61c23a9fce79a38e3dc36d776f2fb9adaed
ruah0807 <kimhk0315@gmail.com>
2024-03-05 22:15:24 +0900

    회원 탈퇴 모달창 및 구현

--- src/main/java/org/startup/diabetes/config/WebSecurityConfig.java
+++ src/main/java/org/startup/diabetes/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.startup.diabetes.security.UserDetailService;
 
