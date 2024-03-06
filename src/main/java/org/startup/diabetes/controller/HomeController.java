e47dad93d60bdd12acdd83fbaa7485c3a59a6624
JaeHyungL2 <150316684+JaeHyungL2@users.noreply.github.com>
2024-03-06 17:45:23 +0900

    [Feat] Board - screen ouput upgrade

--- src/main/java/org/startup/diabetes/controller/HomeController.java
+++ src/main/java/org/startup/diabetes/controller/HomeController.java
@@ -6,7 +6,7 @@ public class HomeController {
     @GetMapping("/")  //기본주소 요청이오면
 
     public String index() {  // 이 메소드가 호출이되고
-        return "index"; //index.html 뷰를 찾아간다. 하도록 만듬 컨트롤러단에서
+        return "/board/index"; //index.html 뷰를 찾아간다. 하도록 만듬 컨트롤러단에서
 
     }
 }
